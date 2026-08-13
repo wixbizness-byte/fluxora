1dae6d7ca7c786fd3f514c0d2d8ca218bf9086a1
meimei_digitals <wixbizness@gmail.com>
2026-08-13 18:42:12 +0800

    Use direct pricing entitlement filter

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,6 +1,5 @@
 import Site from "../site";
 import PricingDraftEnhancer from "./pricing-draft-enhancer";
-import PricingEntitlementFilter from "./pricing-entitlement-filter";
 import styles from "./pricing-page.module.css";
 import themeStyles from "./pricing-main-theme.module.css";
 import draftStyles from "./pricing-draft.module.css";
@@ -9,7 +8,6 @@ export default function PricingPage() {
   return (
     <div className={`${styles.pricingPage} ${themeStyles.mainTheme} ${draftStyles.draft}`}>
       <Site pricingMode />
-      <PricingEntitlementFilter />
       <PricingDraftEnhancer />
     </div>
   );
