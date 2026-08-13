ee8458c3c547e8243bcf39f55d45364cf420746f
meimei_digitals <wixbizness@gmail.com>
2026-08-13 18:04:42 +0800

    Enable pricing UX draft

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,11 +1,14 @@
 import Site from "../site";
+import PricingDraftEnhancer from "./pricing-draft-enhancer";
 import styles from "./pricing-page.module.css";
 import themeStyles from "./pricing-main-theme.module.css";
+import draftStyles from "./pricing-draft.module.css";
 
 export default function PricingPage() {
   return (
-    <div className={`${styles.pricingPage} ${themeStyles.mainTheme}`}>
+    <div className={`${styles.pricingPage} ${themeStyles.mainTheme} ${draftStyles.draft}`}>
       <Site pricingMode />
+      <PricingDraftEnhancer />
     </div>
   );
 }
