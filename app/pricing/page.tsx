658d4cd73e01a3e3a95e90ecba76fe5907872205
meimei_digitals <wixbizness@gmail.com>
2026-08-13 17:54:22 +0800

    Use Fluxora main palette on pricing page

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,9 +1,10 @@
 import Site from "../site";
 import styles from "./pricing-page.module.css";
+import themeStyles from "./pricing-main-theme.module.css";
 
 export default function PricingPage() {
   return (
-    <div className={styles.pricingPage}>
+    <div className={`${styles.pricingPage} ${themeStyles.mainTheme}`}>
       <Site pricingMode />
     </div>
   );
