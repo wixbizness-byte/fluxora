36807ebc6c515a3ceb20087c712bfeedd2709ef0
meimei_digitals <wixbizness@gmail.com>
2026-08-29 23:17:23 +0800

    Use segmented referral dashboard sections

--- app/refer/page.tsx
+++ app/refer/page.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from "next";
 import ReferClient from "./refer-client";
-import ReferralDeepLinkBuilder from "./referral-deep-link-builder";
-import ShareableRewardsPanel from "./shareable-rewards-panel";
-import ReferralTrustPanel from "./referral-trust-panel";
-import MilestonesPanel from "./milestones-panel";
-import ReferralTiersPanel from "./referral-tiers-panel";
-import RewardWalletPanel from "./reward-wallet-panel";
+import ReferralSectionTabs from "./referral-section-tabs";
 
 export const metadata: Metadata = {
   title: "Refer & Earn | Fluxora",
@@ -16,14 +11,7 @@ export default function ReferPage() {
   return (
     <>
       <ReferClient />
-      <ReferralDeepLinkBuilder />
-      <ShareableRewardsPanel />
-      <ReferralTrustPanel />
-      <MilestonesPanel />
-      <ReferralTiersPanel />
-      <div id="reward-wallet">
-        <RewardWalletPanel />
-      </div>
+      <ReferralSectionTabs />
     </>
   );
 }
